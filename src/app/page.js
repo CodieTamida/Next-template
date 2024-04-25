b4ebddf87be5602392f748039bb701e2eeec2980
Codie Tamida <codietamida@gmail.com>
2024-04-25 00:56:26 -0700

    local code

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Navbar from "../../components/nav.jsx";
+
+export default function Home() {
+  return <Navbar></Navbar>;
+}
